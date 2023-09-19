94e43b619023a32532b7becaeb6b41b312e13b7f
Kirilloao <kirilltaraturin747@gmail>
2023-09-19 11:29:37 +0200

    setup tableVIew

--- TaskHub/Controllers/ToDoListViewController.swift
+++ TaskHub/Controllers/ToDoListViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 import SnapKit
 
-final class ToDoListViewController: UIViewController {
+final class ToDoListViewController: UITableViewController {
     
     // MARK: - Private Properties
-    private let listView = ListView()
+    private let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupConstraints()
+        setupNavigationBar()
+        setupTableView()
     }
     
     // MARK: - Private Methods
-    private func setupConstraints() {
-        listView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+    private func setupNavigationBar() {
+        title = "TaskHub"
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        //устанавливаем цвет для navigationBar
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        // меняем цвет для текста
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        // меняем цвет в статичном положении и в скролинге
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
     }
+    
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ToDoListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = itemArray[indexPath.row]
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ToDoListViewController {
 }
 
